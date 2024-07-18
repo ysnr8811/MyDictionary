16277e556840d93a5684f9666d2955dbc4d7be33
ysnr8811 <86781060+ysnr8811@users.noreply.github.com>
2024-07-18 21:52:33 +0900

    文言修正

--- shell/git-add-commit-push.sh
+++ shell/git-add-commit-push.sh
@@ -4,7 +4,7 @@
 git add .
 
 # コミットメッセージの入力を求める
-read -p "コミットメッセージを入力してください: " commit_message
+read -p "コミットメッセージを入力してください※issueに紐づける場合は「#1」のように記載してください: " commit_message
 
 # git commit -m ''
 git commit -m "$commit_message"
