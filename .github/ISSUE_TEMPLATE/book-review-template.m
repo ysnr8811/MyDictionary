ff544d71cb4620868969452c4ba51538f61c5b58
ysnr8811 <86781060+ysnr8811@users.noreply.github.com>
2024-12-19 21:37:13 +0900

    issueテンプレート

---
+++ .github/ISSUE_TEMPLATE/book-review-template.m
@@ -0,0 +1,19 @@
+---
+name: Book Review
+about: 本のレビュー用のテンプレート
+title: "[Book Review] "
+labels: book-review
+assignees:
+---
+
+# タスク
+- [ ] 本の読了
+- [ ] 本のレビュー
+
+# 概要
+
+# YouTubeリンク
+[YouTubeのリンク](https://www.youtube.com/リンクの例)
+
+# Amazonリンク
+[本のリンク](https://www.amazon.co.jp/リンクの例)
